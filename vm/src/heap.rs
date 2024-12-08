ac5d1a6251fbaff658d94e1f1b9d3443b2351ec1
Dragonchu <dr.dragonchu@gmail.com>
2024-12-08 23:59:33 +0800

    feat: refactor

--- vm/src/heap.rs
+++ vm/src/heap.rs
@@ -1,19 +1,22 @@
 use typed_arena::Arena;
 
-use crate::{class::{ArrayKlassDesc, ArrayOopDesc, ArrayOopRef, InstanceOopDesc, InstanceOopRef}, method_area::MethodArea};
+use crate::class::{ArrayOopDesc, ArrayOopRef, InstanceOopDesc, InstanceOopRef};
 
 pub struct Heap<'memory> {
     instance_oops: Arena<InstanceOopDesc<'memory>>,
-    array_oops: Arena<ArrayOopDesc<'memory>>
+    array_oops: Arena<ArrayOopDesc<'memory>>,
 }
 impl<'a> Heap<'a> {
-    pub fn new() -> Heap<'a>{
-       Heap {
-        instance_oops: Arena::new(),
-        array_oops: Arena::new()
-       } 
+    pub fn new() -> Heap<'a> {
+        Heap {
+            instance_oops: Arena::new(),
+            array_oops: Arena::new(),
+        }
     }
-    pub fn allocate_instance_oop(&'a self, instance_oop: InstanceOopDesc<'a>) -> InstanceOopRef<'a> {
+    pub fn allocate_instance_oop(
+        &'a self,
+        instance_oop: InstanceOopDesc<'a>,
+    ) -> InstanceOopRef<'a> {
         self.instance_oops.alloc(instance_oop)
     }
 
